317577ff5a0a7a70b1ebda6413ace0d07007e2bf
Malakai <kayydv1016@gmail.com>
2022-07-01 13:41:47 -0600

    Test3

--- src/components/FakeSiteBanner.tsx
+++ src/components/FakeSiteBanner.tsx
@@ -5,16 +5,18 @@ import {ArrowRightIcon} from '@heroicons/react/outline'
 
 const FakeSiteBanner = () => {
   return (
-    <a href="https://github.com/Principles-Of-Software-Dev/summer-project/tree/master" >
-      <div className='bg-zinc-100 w-screen h-10 drop-shadow-lg flex items-center border-b border-zinc-200 hover:bg-zinc-200 justify-center group mb-4'>
-          
-          <div className='flex items-center text-xs tracking-widest'>
-              Click Here to Check our Repo Out!
+    <div className='h-10'>
+      <a href="https://github.com/Principles-Of-Software-Dev/summer-project/tree/master" >
+        <div className='c_water w-screen drop-shadow-lg flex items-center hover:c_jetstream justify-center group mb-4'>
+            
+            <div className='flex items-center text-xs tracking-widest'>
+                Click Here to Check our Repo Out!
+          </div>
+          {/* add horizontal bounce effect on hover */}
+            <ArrowRightIcon className='transition h-5 m-2 group-hover:translate-x-1' />
         </div>
-        {/* add horizontal bounce effect on hover */}
-          <ArrowRightIcon className='transition h-5 m-2 group-hover:translate-x-1' />
-      </div>
-      </a>
+        </a>
+    </div>
   )
 }
 
