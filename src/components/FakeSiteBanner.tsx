870d249ce59ed369cb835b546ba6d46264bf0021
Malakai <kayydv1016@gmail.com>
2022-07-01 12:53:50 -0600

    added dummy page and route functionality

--- src/components/FakeSiteBanner.tsx
+++ src/components/FakeSiteBanner.tsx
@@ -5,11 +5,11 @@ import {ArrowRightIcon} from '@heroicons/react/outline'
 
 const FakeSiteBanner = () => {
   return (
-    <a href="https://github.com/KayyDVC/Store-Front-Template" >
+    <a href="https://github.com/Principles-Of-Software-Dev/summer-project/tree/master" >
       <div className='bg-zinc-100 w-screen h-10 drop-shadow-lg flex items-center border-b border-zinc-200 hover:bg-zinc-200 justify-center group mb-4'>
           
           <div className='flex items-center text-xs tracking-widest'>
-              Welcome to The This Website
+              Click Here to Check our Repo Out!
         </div>
         {/* add horizontal bounce effect on hover */}
           <ArrowRightIcon className='transition h-5 m-2 group-hover:translate-x-1' />
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FakeSiteBanner from '../components/FakeSiteBanner';
+
+const LandingPage = () => {
+  return (
+      <div>
+          <FakeSiteBanner />
+    </div>
+  )
+}
+
+export default LandingPage;
