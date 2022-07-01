dbd28a222e533de8d06457ed6a1d15cc7b992484
Malakai <kayydv1016@gmail.com>
2022-07-01 11:50:52 -0600

    Added scripts, dependencies, and configs

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      'c_blue_pigment': '#29339B',
+      'c_rich_electric_blue': '#009DDC',
+      'c_jet_stream': '#B3DEC1',
+      'c_water': '#DBF9F0',
+      'c_cultured': '#F7F9F7',
+      'c_metallic_seaweed': '#077187',
+      'c_satin_sheen_gold': '#C8963E',
+      'c_white': '#ffffff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
