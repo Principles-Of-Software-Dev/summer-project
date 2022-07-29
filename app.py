0b154d3d49d31cff22b758475bd2fb896d8144a7
Malakai <kayydv1016@gmail.com>
2022-07-28 23:53:04 -0600

    testing integration

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__, static_folder='build', static_url_path='')
 CORS(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
 #    os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://smzcgkpsvsiscy:fd1612df686a25ec3c875115adbac25133506f7f4d9286f2eb3e077e0349b83d@ec2-44-206-214-233.compute-1.amazonaws.com:5432/d6s79ttvaturdv'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://smzcgkpsvsiscy:fd1612df686a25ec3c875115adbac25133506f7f4d9286f2eb3e077e0349b83d@ec2-44-206-214-233.compute-1.amazonaws.com:5432/d6s79ttvaturdv'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # DATABASE SETUP
