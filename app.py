14cd1917383b74ec48f25d2901399d89b37f5b98
Malakai <kayydv1016@gmail.com>
2022-07-31 11:55:50 -0600

    deploy

--- app.py
+++ app.py
@@ -720,7 +720,7 @@ def get_properties():
 
                                 authorized_properties.append(property_dict)
 
-                return jsonify({"owned_properties": owned_properties, "authorized_properties": authorized_properties})
+                return {"owned_properties": owned_properties, "authorized_properties": authorized_properties}
             else:
                 # no properties under user
                 return jsonify(411)
