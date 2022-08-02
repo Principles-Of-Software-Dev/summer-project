a81512f03e3c7540b17b4b43e6ca740e46b8e0d6
Malakai <kayydv1016@gmail.com>
2022-08-02 17:34:34 -0600

    deploy

--- src/components/lg_components/ItemsList.tsx
+++ src/components/lg_components/ItemsList.tsx
@@ -4,7 +4,7 @@ import Item from '../sm_components/Item'
 
 const ItemsList = ({ items, displayItem, displayItemForm }) => {
 	return (
-		<div className='grid md:grid md:grid-cols-3 grid-cols-1 h-auto w-screen'>
+		<div className='grid md:grid md:grid-cols-3 grid-cols-1 h-auto w-screen mt-3'>
 			{items.map((prop, numb) => {
 				return (
 					<div className='flex items-center justify-center h-main' key={numb}>
