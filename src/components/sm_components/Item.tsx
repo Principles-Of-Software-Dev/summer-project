45ac29f474167d34b7b81122e4279c1311c14fec
Malakai <kayydv1016@gmail.com>
2022-08-02 11:53:59 -0600

    deploy

--- src/components/sm_components/Item.tsx
+++ src/components/sm_components/Item.tsx
@@ -5,6 +5,14 @@ import Button from './Button' ;
 const Item = ({ item, displayItem, displayItemForm }) => {
     
 	console.log(item)
+
+	const keys = {}
+	
+	let i = 0 ;
+	Object.keys(item.photos).map(key => {
+		keys[key] = i
+	})
+
 	const [currIndex, setCurrIndex] = useState(0) ;
 	const maxLength = item.photos.length ;
 
@@ -21,13 +29,13 @@ const Item = ({ item, displayItem, displayItemForm }) => {
 			<div className='h-full w-full p-6 border border-solid rounded-md grid grid-row-2 '>
 				{/* Image */}
 				<div className='row-span-1 '>
-					{item.photos.map((img, index) => {
+					{Object.keys(item.photos).map( key => {
 						return (
 							<div
-								className={index === currIndex ? 'opacity-80 scale-110 ease-linear duration-100' : 'opacity-0'}
-								key={index}
+								className={keys[key] == currIndex ? 'opacity-80 scale-110 ease-linear duration-100' : 'opacity-0'}
+								key={keys[key]}
 							>
-								{index === currIndex && (
+								{keys[key] === currIndex && (
 									<div className='w-full h-full flex items-center justify-center' >
 										<div className='w-full h-full'>
 											<ChevronLeftIcon className='h-xsmall-logo absolute top-[50%] left-2 cursor-pointer select-none' onClick={prevPic} />
@@ -35,7 +43,7 @@ const Item = ({ item, displayItem, displayItemForm }) => {
 											<div className='absolute w-auto p-1 z-1 bg-sky-300 border rounded-md top-3 md:top-4 left-3 md:left-4'>
 												<p className='text-sm tracking-wide '> { item.estimate }</p>
 											</div>
-											<img src={`data:image/png;base64,${img}`} alt={"item.description"} className='w-full h-full p-2' />
+											<img src={`data:image/png;base64,${item.photos[key]}`} alt={"item.description"} className='w-full h-full p-2' />
 										</div>
 									</div>
 								)}
