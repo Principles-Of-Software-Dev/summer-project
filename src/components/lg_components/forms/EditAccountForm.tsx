b0befa2859aa0f10745f9a684e076ce7063db6df
Malakai <kayydv1016@gmail.com>
2022-08-01 21:22:07 -0600

    deploy

--- src/components/lg_components/forms/EditAccountForm.tsx
+++ src/components/lg_components/forms/EditAccountForm.tsx
@@ -14,7 +14,7 @@ import StateField from '../../sm_components/validator_fields/StateField' ;
 
 const EditAccountForm = ({ option, userInfo }) => {
     
-	const { userLogout, user, editUser } = useUser() ;
+	const { userLogout, editUser } = useUser() ;
 
 	const required = option === 'Setup' ? true : false ;      
 
