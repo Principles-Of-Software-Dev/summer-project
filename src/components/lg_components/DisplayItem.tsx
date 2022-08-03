82c4a0dad2f3e99113014c921e28cf040b6d33b6
Malakai <kayydv1016@gmail.com>
2022-08-02 20:45:53 -0600

    deploy

--- src/components/lg_components/DisplayItem.tsx
+++ src/components/lg_components/DisplayItem.tsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react' ;
 import Button from '../sm_components/Button' ;
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline' ;
-import ItemsControl from './ItemsControl' ;
 
 const DisplayItem = ({ item, displayItem, editItem, deleteItem }) => {
 
 	const [currIndex, setCurrIndex] = useState(0) ;
   
-	const media_list = [item.photos + item.videos ]
-	let maxLength = item.photos.length + item.videos.length
+	let maxLength = item.photos.length
 
 	const prevPic = () => {
 		setCurrIndex(currIndex === 0 ? (maxLength - 1) : (currIndex - 1)) ;
@@ -24,7 +22,7 @@ const DisplayItem = ({ item, displayItem, editItem, deleteItem }) => {
 				<div className='h-full w-full p-6 border border-solid rounded-md grid grid-row-2 '>
 					{/* Image */}
 					<div className='row-span-1'>
-						{ media_list.map((media, index) => {
+						{item.photos.map((img, index) => {
 							return (
 								<div
 									className={index === currIndex ? 'opacity-80 scale-110 ease-linear duration-100' : 'opacity-0'}
@@ -38,10 +36,7 @@ const DisplayItem = ({ item, displayItem, editItem, deleteItem }) => {
 												<div className='absolute w-auto p-1 z-1 bg-sky-300 border rounded-md top-3 md:top-4 left-3 md:left-4'>
 													<p className='text-sm tracking-wide '> ${ item.estimate }</p>
 												</div>
-												{ index < 3 ?
-													<img src={`data:image/png;base64,${media}`} alt={"item.description"} className='w-full h-full p-2' /> :
-													<video src={`data:video/mp4;base64,${media}`} className='w-full h-full p-2'  />
-												}
+												<img src={`data:image/png;base64,${img}`} alt={"item.description"} className='w-full h-full p-2' />
 											</div>
 										</div>
 									)}
